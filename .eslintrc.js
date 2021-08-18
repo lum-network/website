e020488104e917dae2058a9c1e368aff4b986482
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-08-18 16:59:10 +0200

    Fixed compilation + code cleaning

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // E.g. "@typescript-eslint/explicit-function-return-type": "off",
         '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
         'react/prop-types': 'off',
         'react-hooks/rules-of-hooks': 'warn',
         'react-hooks/exhaustive-deps': 'error',
