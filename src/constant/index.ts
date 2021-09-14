c1c9d4d0ed84dca7c96c91e4f41bb8662e18a67d
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-09-14 18:40:29 +0200

    Add testnet domain detection

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,14 +1,18 @@
+const isTestEnv = (): boolean => {
+    return window.location.href.includes('localhost') || window.location.href.includes('testnet');
+};
+
+export const LUM_DOMAIN = isTestEnv() ? 'testnet.lum.network' : 'lum.network';
 export const LUM_NETWORK_DOCUMENTATION = 'https://lum-network.gitbook.io';
 export const LUM_EXPLORER_GITHUB = 'https://github.com/lum-network/explorer';
 export const LUM_WALLET_GITHUB = 'https://github.com/lum-network/wallet';
-export const LUM_WALLET = 'https://wallet.lum.network';
-export const LUM_EXPLORER = 'https://explorer.lum.network';
+export const LUM_WALLET = `https://wallet.${LUM_DOMAIN}`;
+export const LUM_EXPLORER = `https://explorer.${LUM_DOMAIN}`;
 export const LUM_NETWORK_GITHUB = 'https://github.com/lum-network';
 export const LUM_TELEGRAM = '';
 export const LUM_TWITTER = '';
 export const LUM_FACEBOOK = '';
 export const LUM_NETWORK_WHITEPAPER = '/whitepaper.pdf';
-export const LUM_TYPEFORM = 'https://sarah2814.typeform.com/to/lHKll5Dy';
 export const MIN_LARGE_DEVICE_WIDTH = 992;
 // @ts-ignore
 export const IS_FIREFOX = typeof InstallTrigger !== 'undefined';
