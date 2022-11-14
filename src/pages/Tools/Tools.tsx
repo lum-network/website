579bb87c3d54d346c9016e41a6b166cf2b38dcdb
Karim Dalaize <karim.dalaize@epitech.eu>
2022-11-14 17:30:20 +0100

    [Updated] Wordings in translations for tools

--- src/pages/Tools/Tools.tsx
+++ src/pages/Tools/Tools.tsx
@@ -23,50 +23,43 @@ const Tools = (): JSX.Element => {
             <div className="container">
                 <img src={toolsIllu} alt="Tools illustration" className="illustration" />
                 <div className="section-content">
-                    <h1 className="section-content-title mt-5">Tools</h1>
+                    <h1 className="section-content-title mt-5">{t('toolsPage.title')}</h1>
                     <div className="row row-cols-2">
                         <div className="col">{t('toolsPage.description1')}</div>
                         <div className="col">{t('toolsPage.description2')}</div>
                     </div>
-                    <h6 className="mb-3">Tools in numbers</h6>
+                    <h6 className="mb-3">{t('toolsPage.numbers.title')}</h6>
                     <div className="row row-cols-4 numbers-container p-4">
                         <div className="col">
                             <div className="py-3">
                                 <div className="stat-number">7M+</div>
-                                <p>Github Stars</p>
+                                <p>{t('toolsPage.numbers.stars')}</p>
                             </div>
                         </div>
                         <div className="col">
                             <div className="py-3">
                                 <div className="stat-number">7M+</div>
-                                <p>Github Commits</p>
+                                <p>{t('toolsPage.numbers.forks')}</p>
                             </div>
                         </div>
                         <div className="col">
                             <div className="py-3">
                                 <div className="stat-number">7M+</div>
-                                <p>Forks</p>
+                                <p>{t('toolsPage.numbers.commits')}</p>
                             </div>
                         </div>
                         <div className="col">
                             <div className="py-3">
                                 <div className="stat-number">7M+</div>
-                                <p>Open source repos</p>
+                                <p>{t('toolsPage.numbers.openSource')}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row section-margin-top">
                     <div className="col-12 col-lg-5 my-auto">
-                        <h2>
-                            Native
-                            <br />
-                            Lum Wallet
-                        </h2>
-                        <p>
-                            The wallet is tailored-suit for the Interchain and has proved his liability with multiple
-                            projects that forked it
-                        </p>
+                        <h2>{t('toolsPage.wallet.title')}</h2>
+                        <p>{t('toolsPage.wallet.description')}</p>
                         <Link
                             link={LUM_WALLET_GITHUB}
                             className="wallet-github-btn d-block scale-anim text-decoration-none py-3 px-4 rounded-pill"
@@ -74,7 +67,7 @@ const Tools = (): JSX.Element => {
                             <span className="ms-3 me-3">
                                 <img src={github} alt="" />
                             </span>
-                            /wallet
+                            {t('toolsPage.wallet.cta')}
                         </Link>
                     </div>
                     <div className="col-12 col-lg-7">
@@ -86,15 +79,8 @@ const Tools = (): JSX.Element => {
                         <img src={explorerBrowser} className="illustration" alt="Lum Wallet in browser" />
                     </div>
                     <div className="col-12 col-lg-5 my-auto">
-                        <h2>
-                            A specific explorer to view
-                            <br />
-                            the specific messages
-                        </h2>
-                        <p>
-                            Specific messages have been developped for the review&rewards use case. You can see them on
-                            our dedicated explorer
-                        </p>
+                        <h2>{t('toolsPage.explorer.title')}</h2>
+                        <p>{t('toolsPage.explorer.description')}</p>
                         <Link
                             link={LUM_EXPLORER_GITHUB}
                             className="wallet-github-btn d-block scale-anim text-decoration-none py-3 px-4 rounded-pill"
@@ -102,29 +88,26 @@ const Tools = (): JSX.Element => {
                             <span className="ms-3 me-3">
                                 <img src={github} alt="" />
                             </span>
-                            /explorer
+                            {t('toolsPage.explorer.cta')}
                         </Link>
                     </div>
                 </div>
             </div>
             <img src={toolsIllu2} className="section-margin-top" alt="" />
             <div className="container use-cases-container">
-                <h1 className="mb-4">Use Cases</h1>
+                <h1 className="mb-4">{t('useCases.title')}</h1>
                 <div className="d-flex flex-lg-row flex-column justify-content-between">
                     <div className="use-case-card">
                         <img src={skr} alt="skeepers-rewards" />
                         <div className="p-4">
-                            <div className="fw-bold fs-3">Skeepers Rewards</div>
+                            <div className="fw-bold fs-3">{t('useCases.dfract.card.title')}</div>
                             <div className="d-flex flex-row justify-content-between mt-2">
-                                <p>
-                                    Companies can now easily provide crypto rewards to their consumers for great
-                                    user-generated content.
-                                </p>
+                                <p>{t('useCases.skr.card.description')}</p>
                                 <NavLink
                                     to={NavigationConstants.SKR}
                                     className="discover-btn scale-anim text-decoration-none py-2 px-3 rounded-pill ms-4"
                                 >
-                                    Discover
+                                    {t('common.discover')}
                                 </NavLink>
                             </div>
                         </div>
@@ -132,17 +115,14 @@ const Tools = (): JSX.Element => {
                     <div className="use-case-card">
                         <img src={dfract} alt="dfract" />
                         <div className="p-4">
-                            <div className="fw-bold fs-3">DFract</div>
+                            <div className="fw-bold fs-3">{t('useCases.dfract.card.title')}</div>
                             <div className="d-flex flex-row justify-content-between mt-2">
-                                <p>
-                                    First Interchain yield earning index with the mission to bring more users to the
-                                    Internet of Blockchains.
-                                </p>
+                                <p>{t('useCases.dfract.card.description')}</p>
                                 <NavLink
                                     to={NavigationConstants.DFRACT}
                                     className="discover-btn scale-anim text-decoration-none py-2 px-3 rounded-pill ms-4"
                                 >
-                                    Discover
+                                    {t('common.discover')}
                                 </NavLink>
                             </div>
                         </div>
