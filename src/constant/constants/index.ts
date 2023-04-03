260f5ce9bc41f25c72471c3b6189691f64f416c4
Thibault Jaillard <thibault.jaillard@gmail.com>
2023-04-03 10:43:35 -0400

    Remove old whitepaper file and update new whitepaper link

--- src/constant/constants/index.ts
+++ src/constant/constants/index.ts
@@ -16,7 +16,7 @@ export const LUM_TWITTER = 'https://twitter.com/lum_network';
 export const LUM_FACEBOOK = 'https://www.facebook.com/lum.network';
 export const LUM_MEDIUM = 'https://medium.com/lum-network';
 export const LUM_WHITELIST_TYPEFORM = 'https://lum-network.typeform.com/whitelist';
-export const LUM_NETWORK_WHITEPAPER = '/whitepaper.pdf';
+export const LUM_NETWORK_WHITEPAPER = 'https://whitepaper.lum.network';
 export const LUM_OSMOSIS = 'https://app.osmosis.zone/?from=ATOM&to=LUM';
 export const LUM_OSMOSIS_LBP_ARTICLE =
     'https://medium.com/lum-network/participate-to-lum-liquidity-pool-2-2-d8af19e94a21';
